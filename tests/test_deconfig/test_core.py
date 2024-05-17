edf91a9fed1e6e092c49e3682d314ae38dd4ad90
Vaibhaw <vaibhawofficial@gmail.com>
2024-05-17 23:44:54 +0530

    Updated Unit Tests (#14)
    Changed a few tests so they can be used as examples.

--- tests/test_deconfig/test_core.py
+++ tests/test_deconfig/test_core.py
@@ -2,6 +2,7 @@
 Unit tests for the `deconfig.core` module
 """
 
+from typing import Type
 from unittest.mock import MagicMock
 
 import pytest
@@ -18,20 +19,20 @@ def fixture_stub_function():
 
 
 class TestAdapterConfig:
-    def test_Should_add_empty_dict_as_adapter_config_attribute_When_initialize_adapter_config_is_called(
+    def test_Should_add_empty_dict_as_adapter_config_attribute_When_initialized(
         self, stub_function
     ):
         FieldUtil.initialize_adapter_configs(stub_function)
-
         assert hasattr(stub_function, "adapter_configs")
         assert getattr(stub_function, "adapter_configs") == {}
 
     def test_Should_return_adapter_configs_When_get_adapter_configs_is_called(
         self, stub_function
     ):
-        setattr(stub_function, "adapter_configs", {"adapter": "config"})
-        result = FieldUtil.get_adapter_configs(stub_function)
-        assert result == {"adapter": "config"}
+        FieldUtil.initialize_adapter_configs(stub_function)
+        config = MagicMock()
+        FieldUtil.upsert_adapter_config(stub_function, AdapterBase, config)
+        assert FieldUtil.get_adapter_configs(stub_function) == {AdapterBase: config}
 
     def test_Should_raise_error_When_get_adapter_configs_is_called_and_adapter_configs_is_not_set(
         self, stub_function
@@ -39,60 +40,60 @@ class TestAdapterConfig:
         with pytest.raises(ValueError):
             FieldUtil.get_adapter_configs(stub_function)
 
+    # noinspection PyTypeChecker
     def test_Should_add_adapter_config_When_upsert_adapter_config_is_called(
         self, stub_function
     ):
-        new_config = MagicMock()
-        existing_adapter_config = MagicMock()
-        setattr(
-            stub_function, "adapter_configs", {existing_adapter_config: MagicMock()}
-        )
-        FieldUtil.upsert_adapter_config(stub_function, AdapterBase, "config")
-        FieldUtil.upsert_adapter_config(
-            stub_function, existing_adapter_config, new_config
-        )
-        adapter_configs = getattr(stub_function, "adapter_configs")
-        assert len(adapter_configs) == 2
-        assert adapter_configs[existing_adapter_config] == new_config
-        assert getattr(stub_function, "adapter_configs")[AdapterBase] == "config"
+        FieldUtil.initialize_adapter_configs(stub_function)
+        adapter_1: Type[AdapterBase] = MagicMock(AdapterBase)
+        adapter_config_1 = MagicMock()
+        FieldUtil.upsert_adapter_config(stub_function, adapter_1, adapter_config_1)
+
+        assert FieldUtil.get_adapter_configs(stub_function) == {
+            adapter_1: adapter_config_1
+        }
+
+        adapter_2: Type[AdapterBase] = MagicMock(AdapterBase)
+        adapter_config_2 = MagicMock()
+        FieldUtil.upsert_adapter_config(stub_function, adapter_2, adapter_config_2)
+        assert FieldUtil.get_adapter_configs(stub_function) == {
+            adapter_1: adapter_config_1,
+            adapter_2: adapter_config_2,
+        }
 
 
 class TestAdapters:
-    def test_Should_return_adapters_When_get_adapters_is_called(self, stub_function):
-        setattr(stub_function, "adapters", ["adapter"])
-        result = FieldUtil.get_adapters(stub_function)
-        assert result == ["adapter"]
-
-    def test_Should_return_none_When_get_adapters_is_called_and_adapters_is_not_set(
-        self, stub_function
-    ):
-        result = FieldUtil.get_adapters(stub_function)
-
-        assert result is None
-
     def test_Should_set_adapters_When_set_adapters_is_called(self, stub_function):
         adapters = [MagicMock()]
         FieldUtil.set_adapters(stub_function, adapters)
         assert getattr(stub_function, "adapters") == adapters
 
+    def test_Should_add_adapter_When_add_adapter_is_called(self, stub_function):
+        adapter = MagicMock()
+        FieldUtil.add_adapter(stub_function, adapter)
+        assert FieldUtil.get_adapters(stub_function) == [adapter]
+
+    def test_Should_return_adapters_When_get_adapters_is_called(self, stub_function):
+        stub_adapter = MagicMock(AdapterBase)
+        FieldUtil.set_adapters(stub_function, [stub_adapter])
+        assert FieldUtil.get_adapters(stub_function) == [stub_adapter]
+
+    def test_Should_return_none_When_get_adapters_is_called_and_adapters_is_not_set(
+        self, stub_function
+    ):
+        assert FieldUtil.get_adapters(stub_function) is None
+
     def test_Should_return_false_When_has_adapters_is_called_and_adapters_is_not_set(
         self, stub_function
     ):
-        result = FieldUtil.has_adapters(stub_function)
-
-        assert result is False
+        assert FieldUtil.has_adapters(stub_function) is False
 
     def test_Should_return_true_When_has_adapters_is_called_and_adapters_is_set(
         self, stub_function
     ):
-        setattr(stub_function, "adapters", ["adapter"])
-        result = FieldUtil.has_adapters(stub_function)
-        assert result is True
-
-    def test_Should_add_adapter_When_add_adapter_is_called(self, stub_function):
-        adapter = MagicMock()
-        FieldUtil.add_adapter(stub_function, adapter)
-        assert getattr(stub_function, "adapters")[0] == adapter
+        adapter = MagicMock(AdapterBase)
+        FieldUtil.set_adapters(stub_function, [adapter])
+        assert FieldUtil.has_adapters(stub_function) is True
 
 
 class TestName:
@@ -103,7 +104,7 @@ class TestName:
 
     def test_Should_return_name_When_get_name_is_called(self, stub_function):
         name = "name"
-        setattr(stub_function, "name", name)
+        FieldUtil.set_name(stub_function, name)
         assert FieldUtil.get_name(stub_function) == name
 
     def test_Should_raise_error_When_get_name_is_called_and_name_is_not_set(
@@ -115,7 +116,7 @@ class TestName:
     def test_Should_return_true_When_has_name_is_called_and_name_is_set(
         self, stub_function
     ):
-        setattr(stub_function, "name", "name")
+        FieldUtil.set_name(stub_function, "name")
         assert FieldUtil.has_name(stub_function) is True
 
     def test_Should_return_false_When_has_name_is_called_and_name_is_not_set(
@@ -136,7 +137,7 @@ class TestValidationCallback:
         self, stub_function
     ):
         callback = MagicMock()
-        setattr(stub_function, "validation_callbacks", [callback])
+        FieldUtil.add_validation_callback(stub_function, callback)
         assert FieldUtil.get_validation_callbacks(stub_function) == [callback]
 
     def test_Should_return_empty_array_When_get_validation_callback_is_called_and_validation_callback_is_not_set(
@@ -163,7 +164,7 @@ class TestOptional:
         assert getattr(stub_function, "optional") is True
 
     def test_Should_return_optional_When_is_optional_is_called(self, stub_function):
-        setattr(stub_function, "optional", True)
+        FieldUtil.set_optional(stub_function, True)
         assert FieldUtil.is_optional(stub_function) is True
 
     def test_Should_return_false_When_is_optional_is_called_and_optional_is_not_set(
@@ -184,8 +185,8 @@ class TestTransformCallback:
         self, stub_function
     ):
         callback = MagicMock()
-        setattr(stub_function, "transform_callbacks", callback)
-        assert FieldUtil.get_transform_callbacks(stub_function) == callback
+        stub_function = FieldUtil.add_transform_callback(stub_function, callback)
+        assert FieldUtil.get_transform_callbacks(stub_function) == [callback]
 
     def test_Should_return_empty_array_When_get_transform_callback_is_called_and_transform_callback_is_not_set(
         self, stub_function
@@ -217,7 +218,7 @@ class TestCacheResponse:
         self, stub_function
     ):
         response = MagicMock()
-        setattr(stub_function, "cached_response", response)
+        FieldUtil.set_cached_response(stub_function, response)
         assert FieldUtil.get_cached_response(stub_function) == response
 
     def test_Should_raise_value_error_When_get_cached_response_is_called_and_cached_response_is_not_set(
@@ -229,7 +230,7 @@ class TestCacheResponse:
     def test_Should_return_true_When_has_cached_response_is_called_and_cached_response_is_set(
         self, stub_function
     ):
-        setattr(stub_function, "cached_response", MagicMock())
+        FieldUtil.set_cached_response(stub_function, MagicMock())
         assert FieldUtil.has_cached_response(stub_function) is True
 
     def test_Should_return_false_When_has_cached_response_is_called_and_cached_response_is_not_set(
@@ -243,11 +244,12 @@ class TestCacheResponse:
         response = MagicMock()
         FieldUtil.set_cached_response(stub_function, response)
         FieldUtil.delete_cached_response(stub_function)
-        assert not hasattr(stub_function, "cached_response")
+        assert FieldUtil.has_cached_response(stub_function) is False
 
     def test_Should_not_raise_error_When_delete_cached_response_is_called_and_cached_response_is_not_set(
         self, stub_function
     ):
+        assert FieldUtil.has_cached_response(stub_function) is False
         FieldUtil.delete_cached_response(stub_function)
 
 
@@ -263,7 +265,7 @@ class TestOriginalFunction:
         self, stub_function
     ):
         original_function = MagicMock()
-        setattr(stub_function, "original_function", original_function)
+        FieldUtil.set_original_function(stub_function, original_function)
         assert FieldUtil.get_original_function(stub_function) == original_function
 
     def test_Should_raise_value_error_When_get_original_function_is_called_and_original_function_is_not_set(
@@ -275,7 +277,7 @@ class TestOriginalFunction:
     def test_Should_return_true_When_has_original_function_is_called_and_original_function_is_set(
         self, stub_function
     ):
-        setattr(stub_function, "original_function", MagicMock())
+        FieldUtil.set_original_function(stub_function, MagicMock())
         assert FieldUtil.has_original_function(stub_function) is True
 
     def test_Should_return_false_When_has_original_function_is_called_and_original_function_is_not_set(
